5eafc6ba5096956f1effa28281591edafc79e5e9
Enrique-mertoe <abutimartin778@gmail.com>
2024-11-29 15:37:46 +0300

    c1

--- smv-config.php
+++ smv-config.php
@@ -2,9 +2,3 @@
 
 const BASE_PATH = __DIR__;
 const SRC = BASE_PATH . "/src";
-const DATABASE_CONFIG = [
-    "host" => "localhost",
-    "user" => "root",
-    "password" => "",
-    "name" => "vdb",
-];
